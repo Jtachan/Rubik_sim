6c08d3d924217a07b3b1659d0f68c063b6fae4a8
Jaime Gonzalez <76098522+Jtachan@users.noreply.github.com>
2024-02-08 18:20:02 +0100

    Update pkg files (#2)
    * Updated all pkg files
    
    * Corrected path

--- src/rubik_sim/__init__.py
+++ src/rubik_sim/__init__.py
@@ -3,5 +3,5 @@ Rubik's cube package, containing classes to mimic the use of a 3x3x3 cube
 """
 __all__ = ["RubiksCube", "Color", "CubeFace", "CubeSection", "MovesNotation"]
 
-from rubik.cube import RubiksCube
-from rubik.notations import Color, CubeFace, CubeSection, MovesNotation
+from .cube import RubiksCube
+from .notations import Color, CubeFace, CubeSection, MovesNotation
